aa6a23784fc4398ac17bac70b1d079f7747541e8
Maxwell Hurst <maxhurst120898@gmail.com>
2025-02-16 13:37:40 -0500

    added User.js model

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must match a valid email address'],
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought',
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+}, {
+  toJSON: {
+    virtuals: true,
+  },
+  id: false,
+});
+
+// Virtual property to get friend count
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('User', userSchema);
+module.exports = User;
